42127c5d3665bde6d2d69e65831fa563bbc81871
princemumo80@gmail.com <princemumo80@gmail.com>
2020-09-28 15:57:21 +0300

    added the toggle function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteFormComponent,
+    QuoteDetailsComponent,
   ],
   imports: [
     BrowserModule,
